56dac299050e15d22d66691bf1b8da7b129de1d3
eviljeks <ijenya548@gmail.com>
2017-10-21 22:15:39 +0300

    add Jacobi method

--- num_meth_2.py
+++ num_meth_2.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import pow
 
 N = 4
+EPS = pow(10, -6)
 
 def sign(x):
 	if x>0:
@@ -40,43 +41,75 @@ np.set_printoptions(threshold=np.nan)
 
 A = np.matrix([[1,-1,1,-1],[-1,5,-3,3],[1,-3,-7,1],[-1,3,1,10]])
 b = np.array([2,-4,-18,-5])
-S = np.zeros((N,N))
-D = np.zeros((N,N))
 
-m = 0
-bs = 0
-boll = True
 
-for i in range(N):
-	for j in range(i, N):
-		if boll:	
-			for q in range(0,i):
-				m += pow(np.abs(S[q,i]),2)*D[q,q]
-				bs +=(S[q,i]*D[q,q]*S[q,j])
-			boll = False
-			D[i,i] = sign(A[i,i] - m)
-			S[i,i] = pow(np.abs(A[i,i] - m),1/2)
-		if j != i:
-			bs = 0
-			for q in range(0,i):
-				bs +=(S[q,i]*D[q,q]*S[q,j])
+def squares_meth():
 
-		if not boll:
-			S[i,j] = (A[i,j] - bs)/(D[i,i]*S[i,i])
-	#print(m,bs)
+	S = np.zeros((N,N))
+	D = np.zeros((N,N))
+	
 	m = 0
 	bs = 0
 	boll = True
-St = S.transpose()
-STD = St.dot(D)
-#x = np.array([0 for i in range(N)])
-print(S)
-print(STD)
-y = gauss_rev(STD, b, False)
-print(y)
-x = gauss_rev(S, y, True)
-print(x)
+	
+	for i in range(N):
+		for j in range(i, N):
+			if boll:	
+				for q in range(0,i):
+					m += pow(np.abs(S[q,i]),2)*D[q,q]
+					bs +=(S[q,i]*D[q,q]*S[q,j])
+				boll = False
+				D[i,i] = sign(A[i,i] - m)
+				S[i,i] = pow(np.abs(A[i,i] - m),1/2)
+			if j != i:
+				bs = 0
+				for q in range(0,i):
+					bs +=(S[q,i]*D[q,q]*S[q,j])
+	
+			if not boll:
+				S[i,j] = (A[i,j] - bs)/(D[i,i]*S[i,i])
+		#print(m,bs)
+		m = 0
+		bs = 0
+		boll = True
+	St = S.transpose()
+	STD = St.dot(D)
+	#x = np.array([0 for i in range(N)])
+	print(S)
+	print(STD)
+	y = gauss_rev(STD, b, False)
+	print(y)
+	x = gauss_rev(S, y, True)
+	print(x)
+	return x
+	
+	
+def jacobi():
+	x0 = np.array([b[i]/A[i,i] for i in range(N)])
+	x1 = np.array([0. for i in range(N)])
+	while np.linalg.norm(x1-x0)>EPS:
+		x0 = x1.copy()
+		x1 = np.array([0. for i in range(N)])
+		for i in range(N):
+			sum = 0
+			for j in range(N):
+				if j == i:
+					continue
+				else:
+					sum -=(A[i,j]/A[i,i])*x0[j]
+			x1[i] = sum + b[i]/A[i,i]
+		print(x1)
+	y1 = np.array([A[0,j] for j in range(N)])
 
+	#print(y1.dot(x1.transpose())-b[0])
+	return x1
 
+def main():
+	x1 = squares_meth()
+	x2 = jacobi()
+	print("solution by squares method: ", x1)
+	print("solution by Jacobi method: ", x2)
 
-#print(A)
+if __name__ == '__main__':
+
+	main()
