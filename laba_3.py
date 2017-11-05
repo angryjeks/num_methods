c91da6bac513137529e80a4472a4fda4d97f25eb
eviljeks <ijenya548@gmail.com>
2017-11-05 21:24:04 +0200

    added output to file, some small fixes (functions returns etc.)

--- laba_3.py
+++ laba_3.py
@@ -17,11 +17,9 @@ def dot_product_method(A, N):
 		x = matrix_mul(A1, e_vec)
 		mu1 = vector_mul(x, e_vec)
 		e_vec = x/norm_vector_fro(x)
-		print(e_vec, mu1)
 		_norm = abs(mu1 - mu)
 		mu = mu1
-	print(mu1, count)
-	return mu1
+	return mu1, count
 
 def find_max_element_up_matrix(A,N):
 	_max = 0
@@ -34,42 +32,44 @@ def find_max_element_up_matrix(A,N):
 	return _max, _max_pair
 
 def jacobi_eigenvalue(A,N):
+	_max_lambda = 0
 	count = 1
 	A1 = A.copy()
 	H = np.eye(N)
 	H1 = np.eye(N)
 	_max, _max_pair = find_max_element_up_matrix(A1,N)
 	while abs(_max) > EPS:
-	#for q in range(5):
 		count +=1
-		#print(_max_pair)
 		phi = 0.5*(np.arctan((2*A1[_max_pair[0], _max_pair[1]])/(A1[_max_pair[0], _max_pair[0]]-A1[_max_pair[1], _max_pair[1]])))
 		sin_phi, cos_phi = np.sin(phi), np.cos(phi)
-		#print(sin_phi, cos_phi)
 		H[_max_pair[0],_max_pair[0]] = cos_phi
 		H[_max_pair[0],_max_pair[1]] = -sin_phi
 		H[_max_pair[1],_max_pair[0]] = sin_phi
 		H[_max_pair[1],_max_pair[1]] = cos_phi
 		H1 = matrix_mul(H1,H)
-		#print(H)
 		A1 = matrix_mul(matrix_mul(H.transpose(), A1), H)
-		#print(A1)
 		H = np.eye(N)
 		_max, _max_pair = find_max_element_up_matrix(A1,N)
-	print(A1)
-	print("count of iterations = ", count)
-	print(H1)
+	for i in range(N):
+		if A1[i,i] >= _max_lambda:
+			_max_lambda = A1[i,i]
+	#print(A1)
+	#print("count of iterations = ", count)
+	#print(H1)
+	return A1, H1, count, _max_lambda
 
 def main():
-	N = 5
-	m = 5
-	"""A = np.array([[5,1,2],
-				  [1,4,1],
-				  [2,1,3]])"""
-	#print(A)
+	N = 10
+	m = 10
 	A, b = build_matrix(N,m)
-	a = dot_product_method(A,N)
-	jacobi_eigenvalue(A,N)
+	_lambda, count_dot_product = dot_product_method(A,N)
+	A1_jacobi, H1_jacobi, count_jacobi, _max_lambda = jacobi_eigenvalue(A,N)
+	with open("output_laba3.txt", 'w') as f:
+		f.write("A = \n" + str(A)+"\n")
+		f.write("Jacobi eigenvalue method results: \n" + "A1 = \n" + str(A1_jacobi) + "\n" + "H1 = \n" + str(H1_jacobi) + "\n" +"count of iterations = " + str(count_jacobi) + "\n")
+		f.write("max lambda by Jacobi method = " + str(_max_lambda)+"\n")
+		f.write("______________________________________________________\n")
+		f.write("Dot product method results: \n" + "max eigenvalue = " + str(_lambda) + "\n" + "count of iterations = " + str(count_dot_product) + "\n")
 
 if __name__ == '__main__':
 	main()
